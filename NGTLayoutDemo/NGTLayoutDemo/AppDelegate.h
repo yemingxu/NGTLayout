e893ee69ba1adfc43ab563a8bfba1fe9684ed25e
nightxu <1204944581@qq.com>
2017-07-21 15:35:14 +0800

    Initial commit

---
+++ NGTLayoutDemo/NGTLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NGTLayoutDemo
+//
+//  Created by JoeXu on 2017/7/21.
+//  Copyright © 2017年 JoeXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NGTLayoutDemo/NGTLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NGTLayoutDemo
+//
+//  Created by JoeXu on 2017/7/21.
+//  Copyright © 2017年 JoeXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
